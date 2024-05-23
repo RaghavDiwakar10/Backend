05b7b27086218feedf6119b61cfced4024bbfddf
raghav <raghavdiwakar777@gmail.com>
2024-05-23 13:45:03 +0530

    Login and Logout added successfully.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import {registerUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { loginUser } from "../controllers/user.controller.js";
+import { logoutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router()
 
@@ -16,6 +19,12 @@ userRouter.route("/register").post(
         }
     ]),
     registerUser)
+
+userRouter.route("/login").post(loginUser)
+
+userRouter.route("/logout").post( verifyJWT, logoutUser)
+
+
 
 
 export default userRouter
